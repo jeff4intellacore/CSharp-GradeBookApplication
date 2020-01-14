1fddbb9c9c73dacc59cc606bda8f8789f5a5ff2d
Jeff Johnson <jeff.johnson@bioeden.com>
2020-01-14 14:21:13 -0600

    Completed module

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(String name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
