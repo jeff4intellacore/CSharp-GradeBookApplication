b2e9b3841e071094e180578c72d16293b6b41b48
Jeff Johnson <jeff.johnson@bioeden.com>
2020-01-15 08:54:59 -0600

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -10,6 +11,38 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(String name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            Char retGrade = 'F';
+
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            Double interval = Students.Count * .2;
+            int myPosition = Students.Where(m => m.AverageGrade > averageGrade).Count();
+
+            if (myPosition <= Math.Round(interval * 1))
+            {
+                retGrade = 'A';
+            }
+            else if (myPosition <= Math.Round(interval * 2))
+            {
+                retGrade = 'B';
+            }
+            else if (myPosition <= Math.Round(interval * 3))
+            {
+                retGrade = 'C';
+            }
+            else if (myPosition <= Math.Round(interval * 4))
+            {
+                retGrade = 'D';
+            }
+
+            return retGrade;
         }
     }
 }
