bda939ed976dea1fab6c77f12a9d257aac53576e
Jeff Johnson <jeff.johnson@bioeden.com>
2020-01-16 09:36:32 -0600

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,7 +8,7 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(String name) : base(name)
+        public RankedGradeBook(String name, bool isWeighted) : base(name, isWeighted)
         {
             Type = GradeBookType.Ranked;
         }
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -7,7 +7,7 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(String name) : base(name)
+        public StandardGradeBook(String name, bool isWeighted) : base(name, isWeighted)
         {
             Type = GradeBookType.Standard;
         }
